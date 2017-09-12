cea66d0eb76544547d2cc5b449de86cf967a52ce
Stephen Parker <steve.parker43@gmail.com>
2017-09-12 13:05:23 +0100

    Adding code

--- src/dali_master.cpp
+++ src/dali_master.cpp
@@ -6,9 +6,9 @@
 /**
  * Constructor.
  */
-Dali_master::Dali_master()
+Dali_master::Dali_master(uint8_t _address)
 {
-  // be sure not to call anything that requires hardware be initialized here, put those in begin()
+  address = _address;
 }
 
 /**
@@ -16,24 +16,131 @@ Dali_master::Dali_master()
  */
 void Dali_master::begin()
 {
-    // initialize hardware
-    Serial.println("called begin");
+	Wire.begin();
+	clearStatusRegister();
+    transmitCommand(TERMINATE_C, BLANK_C);
+    transmitCommand(RESET_C, BLANK_C);
+    transmitCommand(RESET_C, BLANK_C);
 }
 
-/**
- * Example method.
- */
-void Dali_master::process()
+byte Dali_master::transmitCommand(byte cmd1, byte cmd2, bool &reply, byte &reply1, byte &reply2)
 {
-    // do something useful
-    Serial.println("called process");
-    doit();
+	byte a = transmitCommand(cmd1, cmd2);
+	reply = false;
+
+	if (bitRead(a, VALIDREPLY_S) == 1) {
+		reply1 = 0x00;
+		reply2 = 0x00;
+		reply = true;
+
+		if (bitRead(a, REPLY1_S) == 1 || bitRead(a, REPLY2_S) == 1) {
+			getCommandRegister(reply1, reply2);
+		}
+	}
+
+	return a;
 }
 
-/**
-* Example private method
-*/
-void Dali_master::doit()
+byte Dali_master::transmitCommand(byte cmd1, byte cmd2)
 {
-    Serial.println("called doit");
+	// Make sure the command register is clear before continuing
+	clearStatusRegister();
+
+	// Wait until bus is free
+	byte b = getStatus();
+	while (bitRead(b, BUSY_S) == 1) {
+		b = getStatus();
+	}
+
+	Wire.beginTransmission(address);
+	Wire.write(0x01);
+	Wire.write(cmd1);
+	Wire.write(cmd2);
+	Wire.endTransmission();
+	delay(10);
+
+	// Wait until the command has been sent
+	b = getStatus();
+	while (bitRead(b, BUSY_S) == 1) {
+		b = getStatus();
+	}
+
+	// Wait until reply timeframe has passed
+	b = getStatus();
+	while (bitRead(b, REPLYTIMEFRAME_S) == 1) {
+		b = getStatus();
+	}
+
+	// If we have an error (frameerror or overrun)
+	if (bitRead(b, FRAMEERROR_S) == 1 || bitRead(b, OVERRUN_S) == 1) {
+		if (bitRead(b, FRAMEERROR_S) == 1) {
+
+		} else {
+
+		}
+	}
+
+	// If we have a valid reply, wait for the data to be available in the register
+	if (bitRead(b, VALIDREPLY_S) == 1) {
+		while (bitRead(b, REPLY1_S) == 0 && bitRead(b, REPLY2_S) == 0) {
+			b = getStatus();
+		}
+	}
+
+	return b;
+}
+
+void Dali_master::getCommandRegister(uint8_t &byte1, uint8_t &byte2) 
+{
+	// Wait until we have the correct number of bytes
+	while (!getCommandRegisterRaw()) {}
+
+	byte1 = Wire.read();
+	byte2 = Wire.read();
+}
+
+byte Dali_master::getStatus()
+{
+	// Wait until we have the correct number of bytes
+	while (!getStatusRaw()) {}
+
+	return Wire.read();
+}
+
+// PRIVATE METHODS
+void Dali_master::clearStatusRegister()
+{
+	uint8_t a, b;
+	getCommandRegister(a, b);
+}
+
+bool Dali_master::getStatusRaw()
+{
+	Wire.beginTransmission(address);
+	Wire.write(0x00);
+	Wire.endTransmission();
+	delay(10);
+
+	Wire.requestFrom(address, 1);
+
+	return (Wire.available() == 1) ? true : false;
+}
+
+bool Dali_master::getCommandRegisterRaw()
+{
+	Wire.beginTransmission(address);
+	Wire.write(0x01);
+	Wire.endTransmission();
+	delay(10);
+
+	Wire.requestFrom(address, 2);
+
+	return (Wire.available() == 2) ? true : false;
+}
+
+void Dali_master::splitAdd(long input, uint8_t &highbyte, uint8_t &middlebyte, uint8_t &lowbyte)
+{
+	highbyte = input >> 16;
+	middlebyte = input >> 8;
+	lowbyte = input;
 }
--- src/dali_master.h
+++ src/dali_master.h
@@ -5,6 +5,40 @@
 
 // This will load the definition for common Particle variable types
 #include "Particle.h"
+#include "application.h"
+
+#ifndef bitRead
+#define bitRead(value, bit) (((value) >> (bit)) & 0x01)
+#endif
+
+#define ON_C                0b00000101 // Alias for RECALLMAXLEVEL_C
+#define RECALLMAXLEVEL_C    0b00000101 
+#define RECALLMINLEVEL_C    0b00000110
+#define OFF_C               0b00000000 // OFF
+
+#define BLANK_C             0b00000000 // Easier to type
+
+// Specifically related to initialisation
+#define RESET_C             0b00100000 // 0x20
+#define TERMINATE_C         0b10100001 // 0xA1
+#define INITIALISE_C        0b10100101 // 0xA5
+#define RANDOMISE_C         0b10100111 // 0xA7
+#define COMPARE_C           0b10101001 // 0xA9
+#define SEARCHADDRH_C       0b10110001 // 0xB1
+#define SEARCHADDRM_C       0b10110011 // 0xB3
+#define SEARCHADDRL_C       0b10110101 // 0xB5
+#define PROGRAMSHORT_C      0b10110111 // 0xB7
+#define WITHDRAW_C          0b10101011 // 0xAB
+
+// Led Warrior I2C bits
+#define BUSERROR_S 7
+#define BUSY_S 6
+#define OVERRUN_S 5
+#define FRAMEERROR_S 4
+#define VALIDREPLY_S 3
+#define REPLYTIMEFRAME_S 2
+#define REPLY2_S 1
+#define REPLY1_S 0
 
 // This is your main class that users will import into their application
 class Dali_master
@@ -13,21 +47,44 @@ public:
   /**
    * Constructor
    */
-  Dali_master();
+  Dali_master(uint8_t _address);
 
   /**
-   * Example method
+   * Begin
    */
   void begin();
 
   /**
-   * Example method
+   * Transmit a command and optionally wait for a reply
    */
-  void process();
+  byte transmitCommand(byte cmd1, byte cmd2, bool &reply, byte &reply1, byte &reply2);
+  byte transmitCommand(byte cmd1, byte cmd2);
+
+  /**
+   * Get command status
+   */
+  byte getStatus();
 
 private:
+  uint8_t address;
+
   /**
-   * Example private method
+   * Clear status register
    */
-  void doit();
+  void clearStatusRegister();
+
+  /**
+   * Get status register. Returns true when valid data is available
+   */
+  bool getStatusRaw();
+
+  /**
+   * Get command register. Returns true when valid data is available
+   */
+  bool getCommandRegisterRaw();
+
+  /**
+   * Split 24 bit long into three bytes
+   */
+  void splitAdd(long input, uint8_t &highbyte, uint8_t &middlebyte, uint8_t &lowbyte);
 };
